d4fd1276456cc512e36312cad64ab07851186c0c
JinwooKim <leaprealm@gmail.com>
2023-09-05 14:16:54 +0900

    Day 26-2

--- Voxel/Source/Voxel/Define.cpp
+++ Voxel/Source/Voxel/Define.cpp
@@ -70,7 +70,7 @@ const TArray<TArray<FVector2D>> FVoxel::BlockTextureUVs =
 	},
 };
 
-const FIntVector FVoxel::ChunkCount = FIntVector(5, 5, 10);
+const FIntVector FVoxel::ChunkCount = FIntVector(20, 20, 10);
 const FIntVector FVoxel::BlockCount = FIntVector(20, 20, 20);
 const float	FVoxel::BlockSize = 100.f;
 
@@ -82,7 +82,7 @@ const float FVoxel::CoalPercent = 8.f;
 const float FVoxel::IronPercent = 4.f;
 const float FVoxel::GoldPercent = 2.f;
 const float FVoxel::DiamondPercent = 1.f;
-const float FVoxel::CavePercent = 30.f;
+const float FVoxel::CavePercent = 15.f;
 
 /*
 (Scale=0.001000,HeightScale=5.887208,Octaves=8,HeightOffset=20.000000,DrawCutOff=0.000000)
